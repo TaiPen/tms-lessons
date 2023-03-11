4f5f8d7dfd8f4c24b505e2d2965f30f42eede5c1
TaiPen <Ololo_1942>
2023-03-11 17:04:09 +0300

    Phone_book_add

--- homework_14/Phone_book.py
+++ homework_14/Phone_book.py
@@ -13,7 +13,28 @@ def phonebook():
             break
         elif num == 1:
             print('1. Добавте новый контакт: ')
+            first_sub = str(input('Введите Имя '))
+            last_sub = str(input('Введите Фамилию '))
+            numb_sub = str(input('Введите номер телефона '))
+            date_sub = (first_sub, last_sub, numb_sub)
             with sqlite3.connect('Phone_b.db') as connection:
-
-
+                connection.execute('''INSERT INTO book (first_name, last_name, phone_number)
+                                            VALUES (?,?,?)''', date_sub)
         elif num == 2:
+            print('2. Список контактов в алфавитном порядке: ')
+            with sqlite3.connect('Phone_b.db') as connection:
+                books = connection.execute('SELECT * from book ORDER BY last_name')
+                for el in books.fetchall():
+                    print(el)
+        elif num == 3:
+            print('3. Обновите номер контакта: ')
+            last_sub = str(input('Введите Фамилию '))
+            numb_sub = str(input('Введите НОВЫЙ номер телефона '))
+            upd_sub = (numb_sub, last_sub)
+            with sqlite3.connect('Phone_b.db') as connection:
+                connection.execute('''UPDATE book 
+                                    SET phone_number = ?
+                                    WHERE last_name == ?''', upd_sub)
+
+
+phonebook()
